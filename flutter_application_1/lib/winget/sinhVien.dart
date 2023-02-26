3c7fd1f272232c0a44820934c59e8805193b126a
JunyTM <20010784@st.phenikaa-uni.edu.com>
2023-02-26 16:14:17 +0700

    create new work

---
+++ flutter_application_1/lib/winget/sinhVien.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class SinhVien extends StatelessWidget {
+  String ma;
+  String hoVaTen;
+  DateTime ngaySinh;
+  double diemTotNghiep;
+
+  SinhVien({this.ma, this.hoVaTen, this.ngaySinh, this.diemTotNghiep}) {
+    
+  };
+}
